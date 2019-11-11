2490a5feddd184a2669bb77bd79d0bd9134b9009
GaoLei <gaolei20@guazi.com>
2019-11-11 08:15:16 +0800

    完善

--- CustomView/mylibrary/src/main/java/com/java/mylibrary/FloatWindow.java
+++ CustomView/mylibrary/src/main/java/com/java/mylibrary/FloatWindow.java
@@ -1,7 +1,7 @@
 package com.java.mylibrary;
 
-import android.annotation.SuppressLint;
 import android.content.Context;
+import android.graphics.PixelFormat;
 import android.graphics.Point;
 import android.os.Build;
 import android.view.Gravity;
@@ -9,8 +9,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.TextView;
-
-import androidx.annotation.RequiresApi;
 
 public class FloatWindow {
     private WindowManager.LayoutParams mParams = null;
@@ -38,29 +36,30 @@ public class FloatWindow {
             windowManager = (WindowManager) context.getApplicationContext().getSystemService(Context.WINDOW_SERVICE);
         }
 
-        Point size = new Point();
-        windowManager.getDefaultDisplay().getSize(size);
+//        Point size = new Point();
+//        windowManager.getDefaultDisplay().getSize(size);
 
         mParams = new WindowManager.LayoutParams();
+//        mParams.alpha=0.5f;
         mParams.packageName = context.getPackageName();
-        mParams.width = WindowManager.LayoutParams.MATCH_PARENT;
+        mParams.width = DensityUtil.dip2px(context,200);
         mParams.height = WindowManager.LayoutParams.MATCH_PARENT;
         mParams.flags = WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL
                 | WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE | WindowManager.LayoutParams.FLAG_LAYOUT_INSET_DECOR
-                | WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN;
+                | WindowManager.LayoutParams.FLAG_DIM_BEHIND;
         // 设置浮动窗口不可聚焦（实现操作除浮动窗口外的其他可见窗口的操作）
 //        mParams.flags = WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE;
 //        mParams.type = WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY;
-        mParams.type = WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL;
-
+        mParams.type = WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY;
+        mParams.type = (Build.VERSION.SDK_INT >= 19 && Build.VERSION.SDK_INT <= 24) ? WindowManager.LayoutParams.TYPE_TOAST : WindowManager.LayoutParams.TYPE_PHONE;
+        mParams.format = PixelFormat.TRANSLUCENT;
 //      mParams.softInputMode = WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE |
 //                WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN;
         mParams.gravity = Gravity.END | Gravity.TOP;
-
-
+        mParams.dimAmount=0.5f;
         LayoutInflater inflater = (LayoutInflater) context
                 .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        mMenuView = inflater.inflate(R.layout.layout_dialog, null);
+        mMenuView = inflater.inflate(R.layout.layout_window, null);
         mTvCancel = mMenuView.findViewById(R.id.tv_cancel_show);
         windowManager.addView(mMenuView, mParams);
         mTvCancel.setOnClickListener(new View.OnClickListener() {
