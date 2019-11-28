673407d4960cf14d4fb161a00bb65415417c0900
Lei Gao <gaolei@elex-tech.com>
2019-11-28 16:01:07 +0800

    添加仿微信发送语音

--- CustomView/app/src/main/java/com/android/customview/MainActivity.kt
+++ CustomView/app/src/main/java/com/android/customview/MainActivity.kt
@@ -6,9 +6,8 @@ import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.android.customview.activitypart.PartActivity
-import com.android.customview.adapter.DividerItemDecoration
-import com.android.customview.adapter.ItemAdapter
 import com.android.customview.calculator.CalculatorActivity
+import com.android.customview.recorder.RecorderActivity
 import com.android.customview.touchevent.DispatchActivity
 import com.android.customview.touchevent.TextActivity
 import com.android.customview.viewpager.ViewpagerActivity2
@@ -18,11 +17,12 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
 
     val items = arrayOf(
-        "DispatchActivity",
-        "TextActivity",
-        "InfiniteViewPager",
-        "Calculator",
-        "ActivityPart"
+        "DispatchActivity（触摸时间分发机制解析）",
+        "TextActivity（绘制文本解析）",
+        "InfiniteViewPagerActivity（无限循环ViewPager解析）",
+        "CalculatorActivity（利用设计模式设计简单计算器解析）",
+        "PartActivity（透明Activity实现弹出界面占屏幕一部分解析）",
+        "RecorderActivity（仿微信发送语音解析）"
     )
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -47,6 +47,7 @@ class MainActivity : AppCompatActivity() {
                     2 -> startActivity(Intent(this@MainActivity, ViewpagerActivity2::class.java))
                     3 -> startActivity(Intent(this@MainActivity, CalculatorActivity::class.java))
                     4 -> startActivity(Intent(this@MainActivity, PartActivity::class.java))
+                    5 -> startActivity(Intent(this@MainActivity, RecorderActivity::class.java))
                 }
             }
         })
---
+++ CustomView/app/src/main/java/com/android/customview/recorder/RecorderActivity.java
@@ -0,0 +1,138 @@
+package com.android.customview.recorder;
+
+import android.Manifest;
+import android.graphics.drawable.AnimationDrawable;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.customview.R;
+import com.android.customview.recorder.manager.MediaManager;
+import com.android.customview.recorder.view.AudioRecorderButton;
+import com.android.customview.recorder.view.Recorder;
+import com.android.customview.recorder.view.RecorderAdapter;
+import com.tbruyelle.rxpermissions2.Permission;
+import com.tbruyelle.rxpermissions2.RxPermissions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import io.reactivex.functions.Consumer;
+
+
+public class RecorderActivity extends AppCompatActivity {
+
+
+    private ListView mListView;
+    private ArrayAdapter<Recorder> mAdapter;
+    private List<Recorder> mDatas =new ArrayList<>();
+
+    private AudioRecorderButton mAudioRecorderButton;
+    private View mAnimView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recorder);
+
+
+        initView();
+        setListViewAdapter();
+
+        new RxPermissions(this).requestEach(
+                Manifest.permission.RECORD_AUDIO, Manifest.permission.WRITE_EXTERNAL_STORAGE
+        )
+                .subscribe(new Consumer<Permission>() {
+                    @Override
+                    public void accept(Permission permission) throws Exception {
+                        if (permission.granted) {
+// 用户已经同意该权限
+                            Log.e("xnm", "1");
+                        } else if (permission.shouldShowRequestPermissionRationale) {
+// 用户拒绝了该权限，那么下次再次启动时，还会提示请求权限的对话框
+                            Log.e("xnm", "2");
+                        } else {
+// 用户拒绝了该权限，并且选中『不再询问』
+                            Log.e("xnm", "3");
+                        }
+                    }
+                });
+    }
+
+    private void initView(){
+        mListView = findViewById(R.id.id_listview);
+        mAudioRecorderButton = findViewById(R.id.id_recorder_button);
+
+        mAudioRecorderButton.setAudioFinishRecorderListener(new AudioRecorderButton.AudioFinishRecorderListener() {
+            @Override
+            public void onFinish(float seconds, String filePath) {
+                //每完成一次录音
+                Recorder recorder = new Recorder(seconds,filePath);
+                mDatas.add(recorder);
+                //更新adapter
+                mAdapter.notifyDataSetChanged();
+                //设置listview 位置
+                mListView.setSelection(mDatas.size()-1);
+            }
+        });
+    }
+
+    private void setListViewAdapter(){
+        mAdapter = new RecorderAdapter(this, mDatas);
+        mListView.setAdapter(mAdapter);
+
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                //如果第一个动画正在运行， 停止第一个播放其他的
+                if (mAnimView != null) {
+                    mAnimView.setBackgroundResource(R.drawable.adj);
+                    mAnimView = null;
+                }
+                //播放动画
+                mAnimView = view.findViewById(R.id.id_recorder_anim);
+                mAnimView.setBackgroundResource(R.drawable.play_anim);
+                AnimationDrawable animation = (AnimationDrawable) mAnimView.getBackground();
+                animation.start();
+
+                //播放音频  完成后改回原来的background
+                MediaManager.getInstance().playSound(mDatas.get(position).getFilePath(), new MediaPlayer.OnCompletionListener() {
+                    @Override
+                    public void onCompletion(MediaPlayer mp) {
+                        mAnimView.setBackgroundResource(R.drawable.adj);
+                        MediaManager.getInstance().pause();
+//                        AudioManager.getInstance().release();
+                    }
+                });
+            }
+        });
+    }
+
+    /**
+     * 根据生命周期 管理播放录音
+     */
+    @Override
+    protected void onPause() {
+        super.onPause();
+        MediaManager.getInstance().pause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        MediaManager.getInstance().resume();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        MediaManager.getInstance().release();
+    }
+
+
+}
