736d70a74b629d52601bbc9641f291d3e8780527
Lei Gao <756985063@qq.com>
2020-06-28 15:38:58 +0800

    1、优化代码

--- CustomView/app/src/main/java/com/android/customview/MainActivity.kt
+++ CustomView/app/src/main/java/com/android/customview/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.android.customview.activitypart.PartActivity
 import com.android.customview.adapter.DividerItemDecoration
 import com.android.customview.adapter.ItemAdapter
+import com.android.customview.animation.AnimationActivity
 import com.android.customview.calculator.CalculatorActivity
 import com.android.customview.recorder.RecorderActivity
 import com.android.customview.skin.ZipActivity
@@ -32,6 +33,18 @@ class MainActivity : AppCompatActivity() {
         "RecorderActivity（仿微信发送语音解析）"
 
     )
+    val activities = arrayOf(
+        DispatchActivity::class.java,
+        TextActivity::class.java,
+        ViewpagerActivity2::class.java,
+        CalculatorActivity::class.java,
+        PartActivity::class.java,
+        StyleActivity::class.java,
+        ZipActivity::class.java,
+        AnimationActivity::class.java,
+        RecorderActivity::class.java
+
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -50,16 +63,7 @@ class MainActivity : AppCompatActivity() {
         itemAdapter.setOnItemClickLitener(object : ItemAdapter.OnItemClickLitener {
             override fun onItemClick(v: View) {
                 var position = recyclerview.getChildAdapterPosition(v)
-                when (position) {
-                    0 -> startActivity(Intent(this@MainActivity, DispatchActivity::class.java))
-                    1 -> startActivity(Intent(this@MainActivity, TextActivity::class.java))
-                    2 -> startActivity(Intent(this@MainActivity, ViewpagerActivity2::class.java))
-                    3 -> startActivity(Intent(this@MainActivity, CalculatorActivity::class.java))
-                    4 -> startActivity(Intent(this@MainActivity, PartActivity::class.java))
-                    5 -> startActivity(Intent(this@MainActivity, StyleActivity::class.java))
-                    6 -> startActivity(Intent(this@MainActivity, ZipActivity::class.java))
-                    7 -> startActivity(Intent(this@MainActivity, RecorderActivity::class.java))
-                }
+                startActivity(Intent(this@MainActivity, activities[position]))
             }
         })
     }
